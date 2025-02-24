dc78f32adfae539f6383d66f834979104c290550
Hafner <peterhafner552@gmail.com>
2025-02-24 09:33:04 +0100

    initial commit

---
+++ src/main/kotlin/dev/hafnerp/springbootjparest05_hafner/Springbootjparest05HafnerApplication.kt
@@ -0,0 +1,11 @@
+package dev.hafnerp.springbootjparest05_hafner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springbootjparest05HafnerApplication
+
+fun main(args: Array<String>) {
+	runApplication<Springbootjparest05HafnerApplication>(*args)
+}
