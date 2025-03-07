3afb06857b0cfa7bb4b7b8275c87a01995c32429
Hafner <peterhafner552@gmail.com>
2025-03-07 09:10:59 +0100

    Added extended functions

--- routes.md
+++ routes.md
@@ -1,6 +1,8 @@
 # Routes
 
 + User: `/api/users/`
++ UserMails: `/api/users/1/mails/`
++ UserParticipation: `/api/users/2/participations/`
 + Event: `/api/events/`
 + Mail: `/api/mails/`
 + Participation: `/api/participations/`
--- src/main/kotlin/dev/hafnerp/springbootjparest05_hafner/controllers/UserController.java
+++ src/main/kotlin/dev/hafnerp/springbootjparest05_hafner/controllers/UserController.java
@@ -1,8 +1,10 @@
 package dev.hafnerp.springbootjparest05_hafner.controllers;
 
-import dev.hafnerp.springbootjparest05_hafner.models.User;
+import dev.hafnerp.springbootjparest05_hafner.models.*;
+import dev.hafnerp.springbootjparest05_hafner.services.MailServiceImpl;
 import dev.hafnerp.springbootjparest05_hafner.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +16,9 @@ public class UserController {
     @Autowired
     private UserServiceImpl userService;
 
+    @Autowired
+    private MailServiceImpl mailService;
+
     @GetMapping("/")
     public List<User> getAll() {
         return userService.getAllUsers();
@@ -22,6 +27,25 @@ public class UserController {
     @GetMapping("/{id}")
     public User getById(@PathVariable Long id) {
         return userService.getUser(id);
+    }
+
+    @GetMapping("/{id}/mails/")
+    public ResponseEntity<List<Mail>> getMails(@PathVariable Long id) {
+        var user = userService.getUser(id);
+
+        if (user instanceof Company) return ResponseEntity.ok(((Company) user).getMails());
+        else if (user instanceof Admin) return ResponseEntity.ok (((Admin) user).getMails());
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/participations/")
+    public ResponseEntity<List<Participation>> getParticipations(@PathVariable Long id) {
+        var user = userService.getUser(id);
+
+        if (user instanceof Company) return ResponseEntity.ok(((Company) user).getParticipations());
+
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping("/")
